5dc05748c0f38f5937f0b9898f112ad526f49f43
Vivek Thirumoorthy <95617665+mh-Vthirumoorthy@users.noreply.github.com>
2023-07-28 01:08:14 +0530

    Update local_countries.tf with new countries.
    Signed-off-by: Vivek Thirumoorthy <95617665+mh-Vthirumoorthy@users.noreply.github.com>

--- modules/networking/front_door_waf_policy/local_countries.tf
+++ modules/networking/front_door_waf_policy/local_countries.tf
@@ -2,5 +2,11 @@ locals {
   countries = {
     "bahrain"   = "BH"
     "singapore" = "SG"
+    "canada" = "CA"
+    "mexico" = "MX"
+    "india" = "IN"
+    "united states" = "US"
+    "united kingdom" = "GB"
+    "poland" = "PL"   
   }
 }
