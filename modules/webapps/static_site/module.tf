9bd7cf4d757c5aa90e57be9d19a9407c11c711f0
Navin S Sundar <navin.sundar@theelixrlabs.com>
2023-09-25 22:51:41 +0530

    commented identity block on static web app module

--- modules/webapps/static_site/module.tf
+++ modules/webapps/static_site/module.tf
@@ -17,12 +17,13 @@ resource "azurerm_static_site" "static_site" {
   sku_size = var.sku_size
   sku_tier = var.sku_tier
 
-  dynamic "identity" {
+###CLDSVC - to fix Unsupported block type run error - current rover terraform version doesn't support#####
+/*  dynamic "identity" {
     for_each = try(var.identity, null) == null ? [] : [1]
 
     content {
       type         = var.identity.type
       identity_ids = lower(var.identity.type) == "userassigned" ? local.managed_identities : null
     }
-  }
+  }*/
 }
