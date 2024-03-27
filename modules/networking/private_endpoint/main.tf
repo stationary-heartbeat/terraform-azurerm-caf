07ec0e24c82d965ceb11050488427207159e5e13
Navin S Sundar <navin.sundar@theelixrlabs.com>
2024-03-28 01:41:58 +0530

    updated locals - pvt agw module

--- modules/networking/private_endpoint/main.tf
+++ modules/networking/private_endpoint/main.tf
@@ -8,6 +8,19 @@ terraform {
 }
 
 locals {
+  tags = var.base_tags ? merge(
+    var.global_settings.tags,
+    var.tags,
+    try(var.settings.tags, null)
+  ) : try(var.settings.tags, null)
+
+  location = can(var.location) || can(var.settings.region) ? try(var.location, var.global_settings.regions[var.settings.region]) : var.resource_groups[try(var.settings.resource_group.lz_key, var.settings.lz_key, var.client_config.landingzone_key)][try(var.settings.resource_group.key, var.settings.resource_group_key)].location
+
+  resource_group_name = can(var.resource_group_name) && var.resource_group_name != null ? var.resource_group_name : var.resource_groups[try(var.settings.resource_group.lz_key, var.settings.lz_key, var.client_config.landingzone_key)][try(var.settings.resource_group.key, var.settings.resource_group_key)].name
+
+}
+
+/*locals {
   module_tag = {
     "module" = basename(abspath(path.module))
   }
@@ -21,4 +34,4 @@ locals {
     var.resource_groups[var.settings.resource_group.lz_key][var.settings.resource_group.key],
     null
   )
-}
+}*/
