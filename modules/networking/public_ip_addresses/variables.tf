9174481cc97eaeb4b1ec34fd406a6523cfa1190d
navs <106237748+mh-nsundar@users.noreply.github.com>
2026-05-21 19:01:28 +0530

    updated zones variable
    Signed-off-by: navs <106237748+mh-nsundar@users.noreply.github.com>

--- modules/networking/public_ip_addresses/variables.tf
+++ modules/networking/public_ip_addresses/variables.tf
@@ -85,14 +85,25 @@ variable "tags" {
   type        = map(any)
 }
 
+#variable "zones" {
+#  description = "(Optional) The availability zone to allocate the Public IP in. Possible values are 1, 2, 3. Defaults to null."
+#  type        = list
+#  default     = null
+
+#  validation {
+#    condition     = contains(["1", "2", "3"], var.zones)
+#    error_message = "Provide an allowed value as defined in https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/public_ip#availability_zone."
+#  }
+#}
+
 variable "zones" {
-  description = "(Optional) The availability zone to allocate the Public IP in. Possible values are 1, 2, 3. Defaults to null."
-  type        = list
-  default     = null
+  description = "The availability zone to allocate the Public IP in. Possible values are 1, 2, 3. Defaults to null."
+  type        = list(string)
+  nullable    = true
 
   validation {
-    condition     = contains(["1", "2", "3"], var.zones)
-    error_message = "Provide an allowed value as defined in https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/public_ip#availability_zone."
+    condition     = setunion(["1", "2", "3"], var.zones == null ? [""] : var.zones) == toset(["1", "2", "3"])
+    error_message = "Update behavior to mandate specifying the zones in order to avoid accidental resources destructions. https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/public_ip#zone."
   }
 }
 
