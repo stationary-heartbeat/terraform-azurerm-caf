0f2448bf01d73c1cb43dc0b879a87221b6f89210
navs <106237748+mh-nsundar@users.noreply.github.com>
2026-05-21 22:00:06 +0530

    fix to enable all 3 zone in pip
    Signed-off-by: navs <106237748+mh-nsundar@users.noreply.github.com>

--- modules/networking/public_ip_addresses/module.tf
+++ modules/networking/public_ip_addresses/module.tf
@@ -1,6 +1,9 @@
 # Last review :  AzureRM version 2.63.0
 # Ref : https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/virtual_desktop_host_pool
 
+locals {
+  resolved_zones = var.zones != null && length(var.zones) > 0 ? [for z in var.zones : tostring(z)] : var.availability_zone == "Zone-Redundant" ? ["1", "2", "3"] : var.availability_zone == "No-Zone" || var.availability_zone == null ? null : [var.availability_zone]
+}
 resource "azurerm_public_ip" "pip" {
   name                    = var.name
   resource_group_name     = var.resource_group_name
@@ -11,7 +14,7 @@ resource "azurerm_public_ip" "pip" {
   idle_timeout_in_minutes = var.idle_timeout_in_minutes
   domain_name_label       = var.generate_domain_name_label ? var.name : var.domain_name_label
   reverse_fqdn            = var.reverse_fqdn
-  availability_zone       = var.zones
+  zones                   = local.resolved_zones
   tags                    = local.tags
   public_ip_prefix_id     = var.public_ip_prefix_id
   ip_tags                 = var.ip_tags
